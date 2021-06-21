486e1ac0e033f2a2112429f0d7a9894602194a66
Shrestha-bib <83754292+Shrestha-bib@users.noreply.github.com>
2021-06-21 17:59:55 +0200

    new coco architectures addded
    coco_dcgan and coco_dcgan_big

--- embeddings/embeddingsLoaderFactory.py
+++ embeddings/embeddingsLoaderFactory.py
@@ -1,10 +1,11 @@
-from embeddings.fasttext.fasttextLoader import fasttextLoader
+#from embeddings.fasttext.fasttextLoader import fasttextLoader
 from embeddings.transformers.transformerLoader import transformerLoader
 class embeddingsLoaderFactory(object):
 
     @staticmethod
     def embeddingLoader(type):
-        if type == 'fasttext':
-            return fasttextLoader()
-        else:
-            return transformerLoader(type)
+        # if type == 'fasttext':
+        #     return fasttextLoader()
+        # else:
+        #     return transformerLoader(type)
+        return transformerLoader(type)
